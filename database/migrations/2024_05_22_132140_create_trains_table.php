fc5309cd5337f5498a2fa46c1478779d27d9de81
samuelescavello <scavellosamuele23@gmail.com>
2024-05-22 16:12:44 +0200

    migration

--- database/migrations/2024_05_22_132140_create_trains_table.php
+++ database/migrations/2024_05_22_132140_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 255);
+            $table->string('departure_station',255);
+            $table->string('arrival_station',255);
+            $table->string('departure_time', 255);
+            $table->string('arrival_time', 255);
+            $table->string('train_code', 255);
+            $table->string('carriages', 255);
+            $table->string('on_time', 255);
+            $table->string('cancelled', 255);
             $table->timestamps();
         });
     }
