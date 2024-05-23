775716af03df7c9e974a0c10f50c550dea83c10a
samuelescavello <scavellosamuele23@gmail.com>
2024-05-23 18:42:45 +0200

    seeder usando faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -13,73 +14,20 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $trains = [
-            [
-                "agency" => "trenitalia",
-                "departure_station" => "milano",
-                "arrival_station" => "roma",
-                "departure_time" => "10:30",
-                "arrival_time" => "12:30",
-                "train_code" => "005966",
-                "carriages" => 20,
-                "on_time" => "in orario",
-                "cancelled" => "no",
-                "date" => "2024-05-22"
-            ],
-            [
-                "agency" => "trenitalia",
-                "departure_station" => "firenze",
-                "arrival_station" => "cosenza",
-                "departure_time" => "10:30",
-                "arrival_time" => "19:30",
-                "train_code" => "005066",
-                "carriages" => 13,
-                "on_time" => "in orario",
-                "cancelled" => "no",
-                "date" => "2024-05-22"
-            ],
-            [
-                "agency" => "trenitalia",
-                "departure_station" => "foggia",
-                "arrival_station" => "roma",
-                "departure_time" => "10:00",
-                "arrival_time" => "18:30",
-                "train_code" => "994621",
-                "carriages" => 10,
-                "on_time" => "in orario",
-                "cancelled" => "no",
-                "date" => "2024-05-22"
-            ],
-            [
-                "agency" => "trenitalia",
-                "departure_station" => "genova",
-                "arrival_station" => "pescara",
-                "departure_time" => "15:00",
-                "arrival_time" => "17:30",
-                "train_code" => "994377",
-                "carriages" => 20,
-                "on_time" => "in orario",
-                "cancelled" => "cancellato",
-                "date" => "2024-05-22"
-            ]
-
-
-        ];
-
-        foreach ($trains as $train) {
+        for($i = 0; $i < 100; $i++){
             $new_train = new Train();
-            $new_train->agency = $train['agency'];
-            $new_train->departure_station = $train['departure_station'];
-            $new_train->arrival_station = $train['arrival_station'];
-            $new_train->departure_time = $train['departure_time'];
-            $new_train->arrival_time = $train['arrival_time'];
-            $new_train->train_code = $train['train_code'];
-            $new_train->carriages = $train['carriages'];
-            $new_train->on_time = $train['on_time'];
-            $new_train->cancelled = $train['cancelled'];
-            $new_train->date = $train['date'];
+            $new_train->agency = $faker->company();
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_code = $faker->bothify('???-#####');
+            $new_train->carriages = $faker->numberBetween(10, 20);
+            $new_train->on_time = $faker->boolean();
+            $new_train->cancelled = $faker->boolean();
+            $new_train->date = $faker->date();
             $new_train->save();
         }
     }
